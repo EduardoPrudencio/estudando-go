d73e00cc94fe21f21fba0e6611f48962a2c44e22
Eduardo Prudencio <eduardolobo.esp@gmail.com>
2022-08-31 10:58:13 -0300

    Inserindo comentártio para lembrete de como se compila o pacote

--- accesspackage.go
+++ accesspackage.go
@@ -2,6 +2,11 @@
 	INICIALMENTE O ERRO QUE OCORRIA ERA package … is not in GOROOT
 	E PARA RESOLVER BASTOU EXECUTAR O COMANDO
 	go env -w GO111MODULE=off
+
+	PARA COMPILAR O PACOTE PRECISAMOS, NO DIRETÓRIO DO PACOTE, EXECUTAR
+	O COMANDO GO BUILD
+
+	EXP: go build calculadora.go
 */
 
 package main
@@ -13,4 +18,5 @@ import (
 
 func main() {
 	fmt.Println(calculadora.Soma(2, 1))
+	fmt.Println(calculadora.Subtracao(2, 1))
 }
