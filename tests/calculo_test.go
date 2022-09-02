bf57cd4620c528b834fd3dc38976ca3698a6c48f
Eduardo Prudencio <eduardolobo.esp@gmail.com>
2022-09-02 14:05:25 -0300

    Inserindo exemplos de criação de arquivos de relatório com dados dos testes.

--- tests/calculo_test.go
+++ tests/calculo_test.go
@@ -1,6 +1,12 @@
 /*
 	PARA EXECUTAR O TESTE BASTA ACESSAR O DITETÓRIO DO ARQUIVO E EXECUTAR O COMANDO
 	GO TEST
+
+	COMANDO GO TEST -COVER EXIBE A PORCENTAGEM DE COBERTURA DOS TESTES
+	COMANDO GO TEST -COVERPROFILE=RESULTADO.OUT EXIBE A PORCENTAGEM DE COBERTURA DOS TESTES
+	COMANDO GO TOOL COVER  -FUNC=RESULT.OUT EXIBE OS DADOS DO ARQUIVO CRIADO
+	COMANDO GO TOOL COVER  -HTML=RESULT.OUT CRIA UM ARQUIVO COM OS DADOS DE SAÍDA NO FORMATO HTML
+
 */
 
 package tests
@@ -14,7 +20,7 @@ const errorMessage string = "O valor esperado é %v mas o resultado encontrado f
 func TestDeveRetornar10(t *testing.T) {
 	expectedValue := 10.0
 
-	value := Average(10.0, 10)
+	value := Average(10.0, 10.0)
 
 	if value != expectedValue {
 		t.Errorf(errorMessage, expectedValue, value)
