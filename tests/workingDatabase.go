60c8e3e1aebccbbaef03cbdc073eb96484238520
Eduardo Prudencio <eduardolobo.esp@gmail.com>
2022-09-05 09:29:21 -0300

    Criando exemplo de código para acessar um banco de dados mysql

---
+++ tests/workingDatabase.go
@@ -0,0 +1,38 @@
+/*
+	INSTALANDO O DRIVER PARA INTEGRAÇÃO COM MYSQL
+	DOCKER DE CRIAÇÃO DO BANCO DE DADOS MYSQL
+	docker run --name mysql-db -p 3306:3306 -e MYSQL_ROOT_PASSWORD=q6dw@s460e -d mysql
+	go get -u github.com/go-sql-driver/mysql
+*/
+
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func exec(db *sql.DB, sql string) sql.Result {
+	result, err := db.Exec(sql)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return result
+}
+
+func main() {
+	fmt.Println(" Testando")
+	db, err := sql.Open("mysql", "root:q6dw@s460e@/")
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer db.Close()
+
+	exec(db, "create database if not exists cursogo")
+}
